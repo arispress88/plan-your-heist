00fb735c73dc1ceb5f38544b1557658d9509d212
Arnold Rispress <arnold.rispress@gmail.com>
2023-05-31 13:31:15 -0400

    phase 1 complete

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Plan Your Heist!");
+
+        Console.WriteLine("Enter a team member's name:");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Enter the team member's skill level (a positive integer):");
+        int skillLevel = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the team member's courage factor (a decimal between 0.0 and 2.0)");
+        double courageFactor = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("\nTeam Member Information:");
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Skill Level: " + skillLevel);
+        Console.WriteLine("Courage Factor: " + courageFactor);
+
+        Console.ReadLine();
+    }
+}
